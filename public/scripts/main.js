b8f9a174d3275a4fa0e6348690fdd67e2eef67d7
Matt Rich <metropolis_4@hotmail.com>
2015-04-01 18:49:25 -0600

    Initial Commit

---
+++ public/scripts/main.js
@@ -0,0 +1,29 @@
+(function(){
+    var user = document.getElementById('name').value;
+
+    // Tell app your name
+    var button = document.getElementById('theButton');
+    button.addEventListener('click', function(){
+        user = document.getElementById('name').value;
+        var greeting = "Hello " + user + " Write a message below!";
+        document.querySelector('#message').textContent = greeting;
+        button.classList.add('hiding');
+        var input = document.getElementById('name');
+        input.classList.add('hiding');
+    });
+
+    // Make a new message
+    var addButton = document.getElementById('addMemo');
+    addButton.addEventListener('click', function(){
+        var messageBlock = document.querySelector('.message');
+        var newMessageBlock = messageBlock.cloneNode(true);
+        var myMessage = document.getElementById('userMemo').value;
+        var auth = newMessageBlock.firstChild;
+        auth.textContent = "Written by: " + user;
+        auth.nextSibling.textContent = myMessage;
+        newMessageBlock.classList.add('show');
+        newMessageBlock.classList.remove('hiding');
+        document.getElementById('myMessages').appendChild(newMessageBlock);
+        document.getElementById('userMemo').value = '';
+    });
+})();
---
+++ readme.md
@@ -0,0 +1,1 @@
+Pure JavaScript message posting without jQuery
